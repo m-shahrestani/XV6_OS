3461143773584671fb7d054dab3587180973196b
mreza <m.shahrestani@aut.ac.ir>
2021-07-18 23:06:40 +0430

    Add thread_create and thread_join function

---
+++ thread.h
@@ -0,0 +1,27 @@
+#include "param.h"
+#include "syscall.h"
+#include "traps.h"
+#include "fs.h"
+#include "mmu.h"
+
+int
+thread_create(void *arg, void (func)(void*))
+{
+    void *stack= malloc(PGSIZE*2);
+    if((uint)stack % PGSIZE)
+    {
+        stack = stack + (PGSIZE - (uint)stack % PGSIZE);
+    }
+	int clone_pid = clone(arg, stack, func);
+    printf(1,"clone_pid is: %d\n", clone_pid);
+    return clone_pid;
+}
+
+int
+thread_join()
+{
+    void *stack = malloc(sizeof(void*));
+	int join_pid = join(&stack);
+    printf(1,"join_pid is: %d\n", join_pid);
+    return join_pid;
+}
